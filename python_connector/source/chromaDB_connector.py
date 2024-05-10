2cb79da323a18b46800838fa591b7ec1a74cee8e
saitamajaeger <saitamajaeger@protonmail.com>
2024-05-10 16:58:01 +0200

    refactoring, improved validate_metadata, set embedding_api_key as parameter

--- python_connector/source/chromaDB_connector.py
+++ python_connector/source/chromaDB_connector.py
@@ -1,192 +1,170 @@
 import chromadb.utils.embedding_functions as embedding_functions
 import chromadb
 import re
-import os
 from datetime import datetime
 
 
 class chromaDB_connector:
-    def __init__(self):
+    def __init__(self, embedding_api_key: str, embedding_api_base: str = "https://hfu-prompt-improvement.openai.azure.com/", embedding_model_name: str = "Test-Ada"):
+        # creating the embedding function of the deployment
         self.__openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-                        api_key=os.environ.get('EF_API_KEY'), # YOU HAVE TO SET AN ENVIRONEMT VAR TODO as parameter
-                        api_base="https://hfu-prompt-improvement.openai.azure.com/",
-                        api_type="azure",
-                        api_version="2023-05-15",
-                        model_name="Test-Ada"
-                    )
-        self.__client = chromadb.PersistentClient(path="chromadb_persistance")
-        self.__prompt_collection = self.__client.get_or_create_collection(embedding_function=self.__openai_ef, name="prompts")
-        
-        self.allowed_metadata_keys = ["name", "description","author", "models", "tags", "languages", "ratings", "comments"]
-
-
-
-    def create_prompt(self, prompt:str, metadata: dict): 
-        self._validate_metadata(metadata=metadata)
-        
-        # enriching metadat
-        metadata["version"] = self._find_new_version(name=metadata["name"])
-        metadata["date_of_creation"] = datetime.now().strftime('%Y-%m-%dT%H:%M:%S') 
-        
-        self.__prompt_collection.add(
-            documents=[prompt], 
-            metadatas=metadata,
-            ids=[f"{metadata['name']}:{metadata['version']}"]
+            api_key=embedding_api_key,
+            api_base=embedding_api_base,
+            api_type="azure",
+            api_version="2023-05-15",
+            model_name=embedding_model_name
         )
 
+        # getting the client
+        self.__client = chromadb.PersistentClient(path="chromadb_persistance")
 
-    def _validate_metadata(self, metadata):
+        # getting or creating the collection
+        self.__prompt_collection = self.__client.get_or_create_collection(
+            embedding_function=self.__openai_ef, name="prompts")
+
+        # defining the allowed metadata keys for metadata validation
+        self.allowed_metadata_keys = [
+            "name", "description", "author", "models", "tags", "languages", "ratings", "comments"]
+
+    # This function saves prompts to the database
+    def create_prompt(self, prompt: str, metadata: dict):
+        # validate the metadata
+        self.__validate_metadata(metadata=metadata)
+
+        # enriching metadata
+        metadata["version"] = self.__find_new_version(
+            name=metadata["name"])  # dynamically look for the next version
+        metadata["date_of_creation"] = datetime.now().strftime(
+            '%Y-%m-%dT%H:%M:%S')
+
+        # adding the prompt to the collection
+        self.__prompt_collection.add(
+            documents=[prompt],
+            metadatas=metadata,
+            ids=[f"{metadata['name']}:{metadata['version']}"]  # id is unique
+        )
+
+    # This function validates the metadata as defined in the docs
+    def __validate_metadata(self, metadata):
+        # Function that validates fields/keys
+        def validate_field(key, value, type=str, required=False, min_length=2, max_length=50, printable=False):
+            if required and key not in metadata:
+                raise ValueError(f"{key} has to be set in metadata!")
+            if key in metadata:
+                if not isinstance(value, type):
+                    raise ValueError(
+                        f"The {key} has to be of type {str(type)}!")
+                if len(value) < min_length or len(value) > max_length:
+                    raise ValueError(
+                        f"The {key} has to be of length between {min_length} and {max_length}")
+                if printable and not value.isprintable():
+                    raise ValueError(f"The {key} is not printable")
+                if not printable and not re.match(r'^[a-zA-Z\-\'\s]+$', value):
+                    raise ValueError(
+                        f"The {key} can only contain letters, - and '.")
+
+        # Function that validates lists
+        def validate_list(key, value, type=str, required=False, min_length=2, max_length=50, printable=False):
+            if key in metadata:
+                if not isinstance(value, list):
+                    raise ValueError(f"The {key} has to be of type list!")
+                for element in value:
+                    validate_field(key=key, value=element, type=type, required=required,
+                                   min_length=min_length, max_length=max_length, printable=printable)
+
         # check if there are any invalid keys in metadata
         if len([key for key in metadata if key not in self.allowed_metadata_keys]) > 0:
-            raise ValueError(f"You entered invalid keys in the metadata: {str([key for key in metadata if key not in self.allowed_metadata_keys])}!")
-        
-        # name validation
-        if "name" not in metadata:
-            raise ValueError("Name has to be set in metadata!")
-        if type(metadata["name"]) != str:
-            raise ValueError("The name has to be of type str!")
-        if len(metadata["name"]) < 2 or len(metadata["name"]) > 50:
-            raise ValueError("The name has to be of length >2 and < 50")
-        if not re.match(r'^[a-zA-Z\-\'\s]+$', metadata["name"]):
-            raise ValueError("The name can only contain letters, hyphens, apostrophes and spaces")
-        
-        # description validation
-        if "description" in metadata:
-            if type(metadata["description"]) != str:
-                raise ValueError("The description has to be of type str!")
-            if len(metadata["description"]) < 2 or len(metadata["description"]) > 500:
-                raise ValueError("The description has to be of length >2 and < 500")
-            if not metadata["description"].isprintable():
-                raise ValueError("The description is not printable")
-        
-        # author validation
-        if "author" in metadata:
-            if type(metadata["author"]) != str:
-                raise ValueError("The author has to be of type str!")
-            if len(metadata["author"]) < 2 or len(metadata["author"]) > 50:
-                raise ValueError("The author has to be of length >2 and < 50")
-            if not re.match(r'^[a-zA-Z\-\'\s]+$', metadata["author"]):
-                raise ValueError("The author can only contain letters, hyphens, apostrophes and spaces")
-        
-        # models validation
-        if "models" in metadata:
-            if type(metadata["models"]) != list:
-                raise ValueError("The models have to be of type list!")
-            for element in metadata["models"]:
-                if type(element) != str:
-                    raise ValueError("The model elements has to be of type str!")
-                if len(element) < 2 or len(element) > 50:
-                    raise ValueError("The models has to be of length >2 and < 50")
-                if not element.isprintable():
-                    raise ValueError("The models names have to be printable")
-            
-        # tags validation
-        if "tags" in metadata:
-            if type(metadata["tags"]) != list:
-                raise ValueError("The tags have to be of type list!")
-            for element in metadata["tags"]:
-                if type(element) != str:
-                    raise ValueError("The tags elements have to be of type str!")
-                if len(element) < 2 or len(element) > 50:
-                    raise ValueError("The tag has to be of length >2 and < 50")
-                if not element.isprintable():
-                    raise ValueError("The tags have to be printable")
-            
-        # languages validation
-        if "languages" in metadata:
-            if type(metadata["languages"]) != list:
-                raise ValueError("The languages have to be of type list!")
-            for element in metadata["languages"]:
-                if type(element) != str:
-                    raise ValueError("The languages elements have to be of type str!")
-                if len(element) < 2 or len(element) > 50:
-                    raise ValueError("The language has to be of length >2 and < 50")
-                if not element.isprintable():
-                    raise ValueError("The languages have to be printable")
-                
-        # ratings validation
-        if "ratings" in metadata:
-            if type(metadata["ratings"]) != dict:
-                raise ValueError("The ratings have to be of type dict!")
-            # TODO more validating for ratings
-        
-        # comments validation
-        if "comments" in metadata:
-            if type(metadata["comments"]) != list:
-                raise ValueError("The comments have to be of type list!")
-            for element in metadata["comments"]:
-                if type(element) != str:
-                    raise ValueError("The comments elements have to be of type str!")
-                if len(element) < 2 or len(element) > 50:
-                    raise ValueError("The comment has to be of length >2 and < 50")
-                if not element.isprintable():
-                    raise ValueError("The comment is not printable")
+            raise ValueError(
+                f"You entered invalid keys in the metadata: {str([key for key in metadata if key not in self.allowed_metadata_keys])}!")
 
+        # validating each key
+        validate_field("name", metadata.get("name"), required=True)
+        validate_field("description", metadata.get(
+            "description"), max_length=500, printable=True)
+        validate_field("author", metadata.get("author"))
+        validate_list("models", metadata.get("models"), printable=True)
+        validate_list("tags", metadata.get("tags"), printable=True)
+        validate_list("languages", metadata.get("languages"), printable=True)
+        # validate_list("ratings", metadata.get("ratings"), printable=True)  # TODO: add further validation
+        validate_list("comments", metadata.get("comments"),
+                      max_length=500, printable=True)
 
-    def get_prompt_by_vector(self, query:str, filter:dict = {}, top_n:int = 1, compile = True): 
-        
+    # This function uses vector search to query the database by a given query
+    def get_prompt_by_vector(self, query: str, filter: dict = {}, top_n: int = 1, compile=True):
+
         # query prompts by vector
         query_result = self.__prompt_collection.query(
             query_texts=[query],
             n_results=top_n,
             where=filter,
-            include=["metadatas","documents"],
+            include=["metadatas", "documents"],
         )
+
         # compile prompts
         if compile:
             for i, result in enumerate(query_result["documents"][0]):
-                query_result["documents"][0][i] = self._compile_prompt(result)
-        
-        return query_result 
+                query_result["documents"][0][i] = self.__compile_prompt(result)
 
+        return query_result
 
-    def get_prompt_by_metadata(self, filter:dict, top_n:int=1, compile = True):
+    # This function uses filtering of metadata to return relevant prompts
+    def get_prompt_by_metadata(self, filter: dict, top_n: int = 1, compile=True):
+        # get all prompts that follow the set filter
         query_result = self.__prompt_collection.get(
             limit=top_n,
             where=filter,
-            include=["documents","metadatas"],
+            include=["documents", "metadatas"],
         )
+
         # compile prompts
         if compile:
             for i, result in enumerate(query_result["documents"]):
-                query_result["documents"][i] = self._compile_prompt(result)
-        
-        return query_result 
+                query_result["documents"][i] = self.__compile_prompt(result)
 
+        return query_result
 
-    def _compile_prompt(self, prompt:str):#TODO latest
+    # This function replaces prompt specifications ({{name:1}} with the prompt
+    def __compile_prompt(self, prompt: str):
         # search for prompt pieces in prompt
         matches = re.finditer(r"\{{(.*?)\}}", prompt)
 
-        # loop throug all found prompt pieces
+        # loop through all found prompt pieces
         for match in matches:
             original_match = match.group(1)
 
+            # check if the match has not yet been replaced and if there is one : in the match
             if original_match in prompt and original_match.count(":") == 1:
+                # lookup the latest version if latest was used
                 if original_match.split(":")[1] == "latest":
-                    prompt_piece_name = original_match.replace(":latest",":" + str(self._find_new_version(name=original_match.split(":")[0])-1))
+                    prompt_name_and_version = original_match.replace(
+                        ":latest", ":" + str(self.__find_new_version(name=original_match.split(":")[0])-1))
                 else:
-                    prompt_piece_name = original_match
+                    prompt_name_and_version = original_match
 
-                # get prompt piece
+                # get relevant prompt
                 query_result = self.__prompt_collection.get(
-                    ids=[prompt_piece_name]
+                    ids=[prompt_name_and_version]
                 )
+                # if the specified prompt exists
                 if len(query_result["documents"]) > 0:
                     prompt_piece = query_result["documents"][0]
 
-                    # replace prompt piece name with value
-                    prompt = prompt.replace("{{" + original_match + "}}", prompt_piece)
-            
+                    # replace prompt specification with value
+                    prompt = prompt.replace(
+                        "{{" + original_match + "}}", prompt_piece)
+
         return prompt
-    
-    def _find_new_version(self, name:str):
-        all_versions_of_prompt = self.get_prompt_by_metadata(filter = {"name":name}, top_n = 1000, compile = False)
+
+    # This function returns the next version number for a prompt name
+    def __find_new_version(self, name: str):
+        # get all prompts with this name
+        all_versions_of_prompt = self.get_prompt_by_metadata(
+            filter={"name": name}, top_n=1000, compile=False)
+
+        # set version to the highest one found +1
         version = 1
         for prompt_metadata in all_versions_of_prompt["metadatas"]:
             if prompt_metadata["version"] >= version:
-                version = prompt_metadata["version"] +1
-                
+                version = prompt_metadata["version"] + 1
+
         return version
-    
-    
