046eeaeca6c561037bc746d23cca9b7a31c643b4
David Pospisil <pospisil.david.29@gmail.com>
2024-05-28 09:07:39 +0200

    frontend project init

---
+++ frontend/promptManagement/app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Prompt Management Project</h1>
+    </div>
+  );
+}
