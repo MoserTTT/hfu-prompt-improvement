b0328a3e12b4871e82736d20274721435f037296
David Pospisil <pospisil.david.29@gmail.com>
2024-05-28 20:11:45 +0200

    Added styles like fonts and color constants

---
+++ frontend/styles/fonts.js
@@ -0,0 +1,93 @@
+import { StyleSheet } from "react-native";
+
+const fonts = StyleSheet.create({
+
+   displayLarge: {
+      fontFamily: "RobotoRegular",
+      fontSize: 57
+   },
+
+   displayMedium: {
+      fontFamily: "RobotoRegular",
+      fontSize: 45
+   },
+   
+   displaySmall: {
+      fontFamily: "RobotoRegular",
+      fontSize: 36
+   },
+
+
+   headlineLarge: {
+      fontFamily: "RobotoRegular",
+      fontSize: 32
+   },
+
+   headlineMedium: {
+      fontFamily: "RobotoRegular",
+      fontSize: 28
+   },
+
+   headlineSmall: {
+      fontFamily: "RobotoRegular",
+      fontSize: 24
+   },
+
+
+   titleLarge: {
+      fontFamily: "RobotoRegular",
+      fontSize: 22
+   },
+
+   titleMedium: {
+      fontFamily: "RobotoMedium",
+      fontSize: 16,
+      letterSpacing: 0.15
+   },
+
+   titleSmall: {
+      fontFamily: "RobotoMedium",
+      fontSize: 14,
+      letterSpacing: 0.1
+   },
+
+
+   labelLarge: {
+      fontFamily: "RobotoMedium",
+      fontSize: 14,
+      letterSpacing: 0.1
+   },
+
+   labelMedium: {
+      fontFamily: "RobotoMedium",
+      fontSize: 12,
+      letterSpacing: 0.5
+   },
+
+   labelSmall: {
+      fontFamily: "RobotoMedium",
+      fontSize: 11,
+      letterSpacing: 0.5
+   },
+
+
+   bodyLarge: {
+      fontFamily: "RobotoRegular",
+      fontSize: 16,
+      letterSpacing: 0.5
+   },
+
+   bodyMedium: {
+      fontFamily: "RobotoRegular",
+      fontSize: 14,
+      letterSpacing: 0.25
+   },
+
+   bodySmall: {
+      fontFamily: "RobotoRegular",
+      fontSize: 12,
+      letterSpacing: 0.4
+   }
+});
+
+export default fonts;
---
+++ frontend/styles/theme.js
@@ -0,0 +1,9 @@
+const COLORS = {
+    black: "#000000",
+    darkGray: "#686868",
+    gray: "#C8C8C8",
+    lightGray: "#F4F4F4",
+    white: "#FFFFFF"
+ };
+ 
+ export { COLORS };
