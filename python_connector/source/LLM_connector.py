97061e72ced65ebd9778de7f77781a76a87ad9ea
saitamajaeger <saitamajaeger@protonmail.com>
2024-06-23 16:58:33 +0200

    cleaned up code and added comments

--- python_connector/source/LLM_connector.py
+++ python_connector/source/LLM_connector.py
@@ -4,24 +4,40 @@ import requests
 import json
 
 # TODO
-# - external prompt or even in the database
-# immer neu abspeichern nach scoring?
-# alle scorings in einer liste
-# nicht doch in nem seperaten container
+# seperater container?
+
 
 class AzureOpenAIHandler:
-    def __init__(self, api_key: str, endpoint : str = 'https://hfu-prompt-improvement.openai.azure.com/', api_version:str="2024-02-01",deployment_name:str="EvaluateAndExecutePrompts"):
+    """
+    Connects to the Azure deployment and provides LLM execution, LLM guided scoring, and LLM-based improvement.
+
+    Args:
+        api_key (str): API key for the Azure OpenAI embedding deployment.
+        endpoint (str, optional): Base URL for the embedding API. Defaults to "https://hfu-prompt-improvement.openai.azure.com/".
+        api_version (str, optional): API version of the deployment, defaults to "2024-02-01".
+        deployment_name (str, optional): Name of the embedding model to use. Defaults to "EvaluateAndExecutePrompts".
+    """
+
+    def __init__(self, api_key: str, endpoint: str = 'https://hfu-prompt-improvement.openai.azure.com/', api_version: str = "2024-02-01", deployment_name: str = "EvaluateAndExecutePrompts"):
+        """
+        Initializes the AzureOpenAIHandler class.
+
+        Args:
+            api_key (str): API key for the Azure OpenAI embedding deployment.
+            endpoint (str, optional): Base URL for the embedding API. Defaults to "https://hfu-prompt-improvement.openai.azure.com/".
+            api_version (str, optional): API version of the deployment, defaults to "2024-02-01".
+            deployment_name (str, optional): Name of the embedding model to use. Defaults to "EvaluateAndExecutePrompts".
+        """
         self.client = AzureOpenAI(
-            api_key=api_key,  
+            api_key=api_key,
             api_version=api_version,
-            azure_endpoint = endpoint
-            )
-        #self.db_client = chromaDB_connector.chromaDB_connector(embedding_api_key=api_key)
+            azure_endpoint=endpoint
+        )
         self.deployment_name = deployment_name
-        self.prompt_by_metadata_url = "http://127.0.0.1:5000/prompt_by_metadata"   #TODO change to chroma inside docker
-        self.create_prompt_url = "http://127.0.0.1:5000/set_prompt"   #TODO change to chroma inside docker
-        self.eval_templates={
-                "structure": """
+        self.prompt_by_metadata_url = "http://python-connector:5000/prompt_by_metadata"
+        self.create_prompt_url = "http://python-connector:5000/set_prompt"
+        self.eval_templates = {
+            "structure": """
 Your task is to evaluate the structure of a given prompt based on specific criteria. Please score the prompt on a scale from 0 to 5, where each score represents the following:
 
 - **0:** The prompt is completely unclear, lacks coherence, and provides no useful guidance.
@@ -48,102 +64,168 @@ Prompt to be evaluated: {prompt}
 
 **Evaluation:**
 score: """
-            }
+        }
         self.improve_template = """Please improve the following Prompt according to this advice and score, try to keep the key charectaristics of the original prompt. 
         Please do not further elaborate on what you did, simply give the improved prompt.
         Please make the Heading of your new prompt: "Improved Prompt".
         This is the Prompt which you should improve: {prompt}
         
         This is the Advice you should consider for the improvement: {scores}"""
-        
-    def __create_prompt(self, template, **kwargs):
+
+    def __compile_prompt(self, template: str, **kwargs):
+        """
+        Compiles a prompt based on a template and provided variables.
+
+        Args:
+            template (str): The template of the prompt. Use {} to insert kwargs.
+            **kwargs: The variables that can be inserted into the template.
+
+        Returns:
+            str: The compiled prompt.
+        """
         return template.format(**kwargs)
-    
-    def __request_prompt(self, prompt_name_and_id):
-        prompt_name_and_id_split = prompt_name_and_id.split(":") 
-        filter_param = {"$and": [{"name": prompt_name_and_id_split[0]}, {"version": int(prompt_name_and_id_split[1])}]}
+
+    def __request_prompt(self, prompt_name_and_version: str):
+        """
+        Fetches a prompt based on its name and version.
+
+        Args:
+            prompt_name_and_version (str): The name and version of the prompt, e.g., "prompt1:2".
+
+        Returns:
+            response (requests.Response): The response object containing the prompt data.
+        """
+        prompt_name_and_id_split = prompt_name_and_version.split(":")
+
+        # Make REST call to fetch prompt by metadata
+        filter_param = {"$and": [{"name": prompt_name_and_id_split[0]}, {
+            "version": int(prompt_name_and_id_split[1])}]}
         params = {"filter": json.dumps(filter_param)}
         response = requests.get(self.prompt_by_metadata_url, params=params)
+
+        # Response handling
         if response.status_code == 200:
             print("Response:", response.json())
         else:
             print("Failed to get response. Status code:", response.status_code)
         return response
 
-    def call_LLM(self, prompt: str, system_prompt:str = None) -> str:
+    def call_LLM(self, prompt: str, system_prompt: str = None, max_tokens: int = 250) -> str:
+        """
+        Calls the LLM to generate a response based on the provided prompt.
+
+        Args:
+            prompt (str): The user prompt to be sent to the LLM.
+            system_prompt (str, optional): An optional system prompt for context.
+            max_tokens (int, optional): The limit of output tokens. Defaults to 250.
+
+        Returns:
+            str: The LLM response.
+        """
         messages = []
+
+        # Set system prompt if available
         if system_prompt is not None:
             messages = [{"role": "system", "content": system_prompt}]
-            
+
+        # add user prompt
         messages.append({"role": "user", "content": prompt})
-        
+
+        # make request to LLM
         response = self.client.chat.completions.create(
             model=self.deployment_name,
             messages=messages,
-            max_tokens=100
+            max_tokens=max_tokens
         )
         return response.choices[0].message.content
-    
-    def eval_prompt_by_LLM(self, prompt_name_and_id:str):
+
+    def eval_prompt_by_LLM(self, prompt_name_and_id: str):
+        """
+        Evaluates a prompt by its name and version using the LLM.
+
+        Args:
+            prompt_name_and_id (str): The name and version of the prompt, e.g., "rest-prompt:1".
+
+        Returns:
+            dict: A dictionary containing the evaluation scores and the new prompt ID.
+        """
+        # Fetch prompt and metadata
         response = self.__request_prompt(prompt_name_and_id)
         prompt_to_eval = json.loads(response.text)["documents"][0]
         previous_metadata = json.loads(response.text)["metadatas"][0]
-        
-        scores = []
+
+        # Loop through each scoring prompt and collect them in scores
+        scores = [] # ["metric1","score1","advice1","metric2", ...]
         for metric in self.eval_templates:
-            compiled_prompt = self.__create_prompt(template=self.eval_templates[metric], prompt=prompt_to_eval)
+            # Compile the eval prompt and run it
+            compiled_prompt = self.__compile_prompt(
+                template=self.eval_templates[metric], prompt=prompt_to_eval)
             response = self.call_LLM(prompt=compiled_prompt)
-            
+
+            # Check if syntax of the response is correct
             if "advice:" not in response:
                 print(f"evaluation failed for {metric}!")
-                continue 
-            
+                continue
+
+            # Add the results to scores
             response_split = response.split("advice:")
-            scores.append(metric)
-            scores.append(response_split[0].strip())
-            scores.append(response_split[1].strip())
-            
+            scores.append(metric)   # metric name
+            scores.append(response_split[0].strip())    # score
+            scores.append(response_split[1].strip()[:249])  # advice
+
+        # set the ratings in previous metadata
         previous_metadata["ratings"] = scores
-        
+
+        # Remove unnecessary/auto-generated metadata
         previous_metadata.pop("version")
         previous_metadata.pop("date_of_creation")
-        
+
+        # Create new version of prompt by making a REST request
         body = {
-            "prompt":prompt_to_eval,
-            "metadata":previous_metadata  
+            "prompt": prompt_to_eval,
+            "metadata": previous_metadata
         }
-        
         response = requests.post(self.create_prompt_url, json=body)
 
+        # Response handling
         if response.status_code == 200:
             print("Response:", response.json())
         else:
             print("Failed to get response. Status code:", response.status_code)
             print("Response:", response.text)
-        
+
+        # Return the new name and id and the scores
         prompt_name_and_id_split = prompt_name_and_id.split(":")
         return_dict = {
-            scores[0] : [scores[1],scores[2]],
+            scores[0]: [scores[1], scores[2]],
             "id": prompt_name_and_id_split[0] + ":" + str(int(prompt_name_and_id_split[1].strip())+1)
         }
         return return_dict
-    
-    def improve_prompt(self, prompt_name_and_id:str):
+
+    def improve_prompt(self, prompt_name_and_id: str):
+        """
+        Improves a prompt by its name and version using the LLM and the provided ratings.
+
+        Args:
+            prompt_name_and_id (str): The name and version of the prompt, e.g., "rest-prompt:1".
+
+        Returns:
+            str: The improved prompt.
+        """
+        # Get the prompt and metadata
         response = self.__request_prompt(prompt_name_and_id)
         user_prompt = json.loads(response.text)["documents"][0]
         user_metadata = json.loads(response.text)["metadatas"][0]
-        if "ratings" not in user_metadata:
-            raise ValueError("Prompt has no ratings")
-        ratings = user_metadata["ratings"][2]
-        improve_prompt = self.__create_prompt(self.improve_template,prompt=user_prompt, scores=ratings)
-        improved_response = self.call_LLM(prompt=improve_prompt)       
-        return improved_response
         
-
-# Usage example:
-if __name__ == "__main__":
-    api_key = os.getenv('EF_API_KEY')
-
-    handler = AzureOpenAIHandler(api_key)
-    result = handler.eval_prompt_by_LLM("rest-prompt:1")
-    print(result)
+        # Check if ratings exist 
+        if "ratings" not in user_metadata:
+            self.eval_prompt_by_LLM(prompt_name_and_id = prompt_name_and_id)
+            
+        # Compile the improvement Prompt with the ratings
+        ratings = user_metadata["ratings"][2]
+        improve_prompt = self.__compile_prompt(
+            self.improve_template, prompt=user_prompt, scores=ratings)
+        
+        # Return LLM response
+        improved_response = self.call_LLM(prompt=improve_prompt)
+        return improved_response
