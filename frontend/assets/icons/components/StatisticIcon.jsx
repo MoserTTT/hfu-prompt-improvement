edd45b1785eed8bfba803231ad66548dc07db480
David Pospisil <pospisil.david.29@gmail.com>
2024-06-30 11:13:20 +0200

    Fixed icon error caused by adding new icon-components

--- frontend/assets/icons/components/StatisticIcon.jsx
+++ frontend/assets/icons/components/StatisticIcon.jsx
@@ -2,10 +2,10 @@
 const StatisticIcon = ({ color, width, height}) => {
     return(
         <svg width={width} height={height} viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M14 17.5L14 28" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M21 21V28" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M28 14V28" stroke={color} stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <rect x="5.25" y="7" width="31.5" height="28" rx="2" stroke={color} stroke-width="2"/>
+            <path d="M14 17.5L14 28" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M21 21V28" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M28 14V28" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <rect x="5.25" y="7" width="31.5" height="28" rx="2" stroke={color} strokeWidth="2"/>
         </svg>
     );
 }
